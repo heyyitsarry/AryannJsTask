66ab7ef1cd07380dd30494945987d3f54ae456d6
heyyitsarry <aryann.chitnis@gmail.com>
2022-08-27 02:38:07 +0530

    JS Task 5

--- task5/task5.js
+++ task5/task5.js
@@ -21,3 +21,96 @@ const results = arr.filter(element => {
 });
 
 console.log(results);
+
+// 4
+const cricket = [{
+  name: 'Sachin Tendulkar',
+  nationality: 'India',
+  retired: true
+  }, {
+  name: 'Shane Warne',
+  nationality: 'Australia',
+  retired: true
+  }, {
+  name: 'Virat Kohli',
+  nationality: 'India',
+  retired: false
+  }];
+  let retiredarray = [];
+  for(let i=0; i<cricket.length; i++)
+  {
+    for(item in cricket[i])
+    {
+      if(cricket[i].retired == false)
+      {
+        retiredarray.push(cricket[i]);
+      }
+      break;
+    }
+  }
+  console.log(retiredarray);
+
+  // 6
+  function findit(value, arr) {
+    var status = 'The number does not exist in the array';
+
+    for (let i = 0; i < arr.length; i++) {
+        let name = arr[i];
+        if (name == value) {
+            status = 'The number exists in the array';
+            break;
+        }
+    }
+
+    return status;
+}
+console.log(findit(6, [1,2,3,4,5]));
+
+// 7
+const arraynew = [1, 2, 3]
+let arraynewupdated = []
+for(i in arraynew)
+{
+  arraynewupdated.push(arraynew[i]);
+  if(i==arraynew.length-2)
+  {
+    break;
+  }
+}
+console.log(arraynewupdated);
+
+// 8
+const array1 = [1,2,3,4,5,6,7,8,9]
+let count=0;
+for(i in array1)
+{
+  if(array1[i]>5)
+  {
+    count++;
+    break;
+  }
+}
+if(count>0)
+{
+  console.log("Yes, there exists an element greater than 5 in the array")
+}
+
+// 9
+const obj1 = {
+  'testable': true,
+  'removable': false,
+  'printable': false,
+  'changeable': true
+  }
+let obj2 = {...obj1}
+for(let i=0; i<=obj2.length; i++)
+{
+  for(item in obj2)
+  {
+    if(i==obj2.length)
+    {
+      obj2[i]="completed";
+    }
+  }
+}
+console.log(obj2)
